0dabd261533247d4445531142595084fd74be56f
Osvaldo Garcia <osvaldo.garcia.mail@gmail.com>
2015-05-05 11:25:34 -0700

    Completed Part 11

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -6,15 +6,31 @@ namespace MvcMovie.Models
 {
     public class Movie
     {
+        /// <summary>
+        /// Added Part 10
+        /// The Movie class to take advantage of the built-in Required, StringLength,  
+        /// RegularExpression, and Range validation attributes.
+        /// </summary>
         public int ID { get; set; }
+        [StringLength( 60, MinimumLength = 3 )]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display( Name = "Release Date" )]
+        [DataType( DataType.Date )]
+        [DisplayFormat( DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true )]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression( @"^[A-Z]+[a-zA-Z''-'\s]*$" )]
+        [Required]
+        [StringLength( 30 )]
         public string Genre { get; set; }
+
+        [Range( 1, 100 )]
+        [DataType( DataType.Currency )]
         public decimal Price { get; set; }
+
+        [RegularExpression( @"^[A-Z]+[a-zA-Z''-'\s]*$" )]
+        [StringLength( 5 )]
         public string Rating { get; set; }//added part in part 9
     }
 
